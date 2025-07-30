02138ea18c3d91cf5d0db578cbd4e76459d0ad59
군고구마 <eunseo0704319@gmail.com>
2025-07-30 14:57:32 +0900

    두 번째 커밋: 나이 변수 출력 코드 추가

--- JavaPractice/src/Main.java
+++ JavaPractice/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Java!");
+        int age = 20; // 정수형 변수 age에 20을 넣음
+        System.out.println(age); // 변수 age의 값을 출력함
     }
 }
