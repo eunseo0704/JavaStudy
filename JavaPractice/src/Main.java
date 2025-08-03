26cf66253283f01e6a960f2eb7e7f09ea584154b
군고구마 <eunseo0704319@gmail.com>
2025-08-03 23:52:57 +0900

    다섯 번째 커밋: 성적 등급 판별기

--- JavaPractice/src/Main.java
+++ JavaPractice/src/Main.java
@@ -4,13 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("숫자를 입력하세요: ");
-        int num = scanner.nextInt();
+        System.out.print("점수를 입력하세요: ");
+        int score = scanner.nextInt();
 
-        if (num == 0) {
-            System.out.println("입력한 숫자는 짝수입니다.");
+        if (score >= 90) {
+            System.out.println("당신의 성적은 A입니다.");
+        } else  if (score >= 80) {
+            System.out.println("당신의 성적은 B입니다.");
+        } else  if (score >= 70) {
+            System.out.println("당신의 성적은 C입니다.");
+        } else  if (score >= 60) {
+            System.out.println("당신의 성적은 D입니다.");
         } else {
-            System.out.println("입력한 숫자는 홀수입니다.");
+            System.out.println("당신의 성적은 F입니다.");
         }
 
         scanner.close();
