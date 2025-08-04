809ba3bc87ed19d3e47918cf6849d9da3567da58
군고구마 <eunseo0704319@gmail.com>
2025-08-04 13:04:24 +0900

    여섯 번째 커밋: 숫자 맞추기 게임 추가

--- JavaPractice/src/Main.java
+++ JavaPractice/src/Main.java
@@ -1,22 +1,30 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // 랜덤 숫자 생성
+        Random random = new Random();
+        int target = random.nextInt(100) + 1; // 1~100 사이 숫자
+
         Scanner scanner = new Scanner(System.in);
+        int guess = 0;
+        int attempts = 0;
 
-        System.out.print("점수를 입력하세요: ");
-        int score = scanner.nextInt();
+        System.out.print("숫자 맞추기 게임을 시작합니다! (1~100)");
 
-        if (score >= 90) {
-            System.out.println("당신의 성적은 A입니다.");
-        } else  if (score >= 80) {
-            System.out.println("당신의 성적은 B입니다.");
-        } else  if (score >= 70) {
-            System.out.println("당신의 성적은 C입니다.");
-        } else  if (score >= 60) {
-            System.out.println("당신의 성적은 D입니다.");
-        } else {
-            System.out.println("당신의 성적은 F입니다.");
+        while (guess != target) {
+            System.out.print("숫자를 입력하세요: ");
+            guess = scanner.nextInt();
+            attempts++;
+
+            if (guess < target) {
+                System.out.println("더 큰 수를 입력하세요!");
+            } else if (guess > target) {
+                System.out.println("더 작은 수를 입력하세요!");
+            } else {
+                System.out.println("정답입니다! 시도 횟수:" + attempts + "번");
+            }
         }
 
         scanner.close();
